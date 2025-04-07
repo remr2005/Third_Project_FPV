cfbd356338cf80956cb9eb15da6e051c3b65b32f
Kemran <morpeh2005ka@gmail.com>
2025-04-07 22:59:26 +0300

    У А СВО

--- scences/secret_weapon/secret_weapon.gd
+++ scences/secret_weapon/secret_weapon.gd
@@ -4,6 +4,17 @@ extends RigidBody3D
 func _ready():
 	freeze = true  # Замораживаем объект
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		freeze = false  # Размораживаем — объект начнет падать
+#func _process(delta):
+	#if Input.is_action_just_pressed("ui_accept"):
+		#freeze = false  # Размораживаем — объект начнет падать
+
+func detach_from_parent():
+	var global_pos = global_transform
+
+	var root = get_tree().current_scene
+	get_parent().remove_child(self)
+	root.add_child(self)
+
+	global_transform = global_pos
+	if self is RigidBody3D:
+		freeze = false
