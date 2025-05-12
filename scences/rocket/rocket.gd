c3a525b54e976c0cdd5abb97571da46ad8b4fce2
UltraUberTurk <morpeh2005ka@gmail.com>
2025-05-12 09:58:49 +0300

    Добавил простую ракету

---
+++ scences/rocket/rocket.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+@export var turn_speed = 3
+var target: Node3D = null
+var speed = 30
+
+func _ready():
+	# Подключаемся к сигналу области "зрения"
+	$Vision3D.connect("body_entered", _on_body_entered)
+	$Vision3D.connect("body_exited", _on_body_exited)
+
+func _physics_process(delta):
+	velocity = -transform.basis.z * speed
+
+	if target:
+		var to_target = (target.global_position - global_position).normalized()
+		var forward = -transform.basis.z.normalized()
+		var new_dir = forward.lerp(to_target, delta * turn_speed).normalized()
+		look_at(global_position + new_dir, Vector3.UP)
+	move_and_slide()
+
+func _on_body_entered(body):
+	if body.name == "dron":
+		target = body
+
+func _on_body_exited(body):
+	if body == target:
+		target = null
